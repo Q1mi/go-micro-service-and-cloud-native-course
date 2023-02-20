202369381d1aa59b6262218d2dd6fe7d0137cc2a
李文周 <liwenzhou7@gmail.com>
2023-02-20 23:32:59 +0800

    feat: add lesson61

---
+++ lesson61/mall/service/user/api/internal/handler/signupHandler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"api/internal/logic"
+	"api/internal/svc"
+	"api/internal/types"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func SignupHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.SignupRequest
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := logic.NewSignupLogic(r.Context(), svcCtx)
+		resp, err := l.Signup(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
---
+++ lesson61/mall/service/user/api/internal/svc/serviceContext.go
@@ -0,0 +1,29 @@
+package svc
+
+import (
+	"api/internal/config"
+	"mall/service/user/model"
+
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
+
+type ServiceContext struct {
+	Config config.Config
+
+	UserModel model.UserModel // 加入User表增删改查操作Model
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	// UserModel -> 接口类型
+	// *defaultUserModel 实现了接口
+	// 调用构造函数得到 *model.defaultUserModel
+	// NewUserModel(conn sqlx.SqlConn)
+	// 需要 sqlx.SqlCon 的数据库连接
+
+	sqlxConn := sqlx.NewMysql(c.Mysql.DataSource)
+
+	return &ServiceContext{
+		Config:    c,
+		UserModel: model.NewUserModel(sqlxConn, c.CacheRedis),
+	}
+}
